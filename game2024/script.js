0a8b5a77b6c1e6cdefc1f0ffa1ff0461f4b60b42
J. M. <166871908+TheWatermelonMan3@users.noreply.github.com>
2025-01-28 14:17:56 -0500

    Copy game loop from Spicy Yoghurt

--- game2024/script.js
+++ game2024/script.js
@@ -1,1 +1,35 @@
 console.log("Script works");
+
+//The game loop code is from the source: https://spicyyoghurt.com/tutorials/html5-javascript-game-development/create-a-proper-game-loop-with-requestanimationframe
+let secondsPassed;
+let oldTimeStamp;
+let fps;
+
+function gameLoop(timeStamp) {
+
+    // Calculate the number of seconds passed since the last frame
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    // Calculate fps
+    fps = Math.round(1 / secondsPassed);
+
+    // Draw number to the screen
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, 200, 100);
+    context.font = '25px Arial';
+    context.fillStyle = 'black';
+    context.fillText("FPS: " + fps, 10, 30);
+
+    // Perform the drawing operation
+    draw();
+
+    // The loop function has reached it's end. Keep requesting new frames
+    window.requestAnimationFrame(gameLoop);
+}
+
+function draw(){
+  let randomColor = Math.random() > 0.5? '#ff8080' : '#0099b0';
+        context.fillStyle = randomColor;
+        context.fillRect(100, 50, 200, 175);
+}
